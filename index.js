715d968a7704dfcfbaa02bd797332b6aa883dca1
Ichii <45918948+IchiiDev@users.noreply.github.com>
2020-12-09 18:08:18 +0100

    Started impletation of the /links/:link_id endpoint

--- index.js
+++ index.js
@@ -109,6 +109,15 @@ class shortio {
     }
 
     /**
+     * This functions updates a link from the specified domain to change it's params. Endpoint: POST https://api.short.io/links/:link_id
+     * @param {number} id This is the ID of the link you want to update.
+     * @param {Object} linkObject This is the new link object of the updated link.
+     */
+    updateLink(id, linkObject) {
+        if (!linkObject.originalURL) throw new Errror("originalURL is not defined");
+    }
+
+    /**
      * This functions archive a link from the specified domain. Endpoint: POST https://api.short.cm/links/archive
      * @param {number} link_id The ID of the link you want to archive.
      * @returns {Promise<Object>} Request response object into a promise.
